3661a1d1fda8d624fada680c5750e0b9f6105345
vijaysaradhi1994 <34361134+vijaysaradhi1994@users.noreply.github.com>
2018-01-09 23:40:04 +0800

    add direction

--- utilities.py
+++ utilities.py
@@ -6,3 +6,17 @@ def gravityFactory(towards=SCREEN_CENTRE,
                    gravityScale = 1.0, 
 #                   strength= 0.0,  
                    unitDistanceInPixels = 600.0, power =2.0):
+    scalingLimit =1200
+    def g(p):
+        if abs(gravityScale) <FPERROR:  # just dealing with floating point errors to ensure really zero gravity
+            return (0, 0) # no gravity
+        direction = towards-p
+        d = direction.get_length()/unitDistanceInPixels
+        scalingFactor = strength*gravityScale/(d**power)
+        if scalingFactor > scalingLimit:
+            scalingFactor = scalingLimit
+
+        direction = direction.normalized()*scalingFactor
+        
+        return direction
+    return g
