6ef92e19fed7cdfd6147db7adfa144f956939288
vijaysaradhi1994 <34361134+vijaysaradhi1994@users.noreply.github.com>
2018-01-09 23:38:11 +0800

    initial gravityFactory function

---
+++ utilities.py
@@ -0,0 +1,8 @@
+from constants import *
+FPERROR = 1E-5
+
+def gravityFactory(towards=SCREEN_CENTRE, 
+                   strength=10.0,  
+                   gravityScale = 1.0, 
+#                   strength= 0.0,  
+                   unitDistanceInPixels = 600.0, power =2.0):
